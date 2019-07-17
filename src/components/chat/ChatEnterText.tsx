a11e7e7e3ef92e808a1621ded80fd4e87878acc7
Roberto De la Parra <r.delaparralombana@reply.de>
2019-07-17 22:56:50 +0200

    removed unused styles

--- src/components/chat/ChatEnterText.tsx
+++ src/components/chat/ChatEnterText.tsx
@@ -10,33 +10,13 @@ import { IMessagePayload } from '../../entities/request/MessagePayload';
 
 
 const useStyles = makeStyles(theme => ({
-  text: {
-    padding: theme.spacing(2, 2, 0),
-  },
-  paper: {
-    paddingBottom: 50,
-  },
-  list: {
-    marginBottom: theme.spacing(2),
-  },
-  subheader: {
-    backgroundColor: theme.palette.background.paper,
-  },
   appBar: {
     top: 'auto',
     bottom: 0,
   },
   grow: {
     flexGrow: 1,
-  },
-  fabButton: {
-    position: 'absolute',
-    zIndex: 1,
-    top: -30,
-    left: 0,
-    right: 0,
-    margin: '0 auto',
-  },
+  }
 }));
 
 export const ChatEnterText = () => {
