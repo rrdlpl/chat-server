a51532c9ca681ab85aea62f2247d72171c847664
Roberto De la Parra <r.delaparralombana@reply.de>
2019-07-19 19:15:43 +0200

    Added conversation to reducer

--- src/store/chat/reducer.ts
+++ src/store/chat/reducer.ts
@@ -1,13 +1,15 @@
-import { CONNECTED, COMMAND_RECEIVED, MESSAGE_RECEIVED } from './constants';
+import { CONNECTED, COMMAND_RECEIVED, MESSAGE_RECEIVED, MESSAGE_SENT, COMMAND_SENT } from './constants';
 import { IMessageResponse } from '../../entities/responses/MessageResponse';
 
 import { ICommandResponse } from '../../entities/responses/CommandResponse';
 import { IChatAction } from './actions';
+import { IMessagePayload } from '../../entities/request/MessagePayload';
 
 
 const initialState: IChatReducer = {
     messages: [],
     commands: [],
+    conversation: [],
     lastCommand: undefined,
     connected: false,
     socket: undefined
@@ -16,6 +18,7 @@ const initialState: IChatReducer = {
 interface IChatReducer {
     messages: IMessageResponse[]
     commands: ICommandResponse[]
+    conversation: Array<any>,
     lastCommand?: ICommandResponse
     connected: boolean,
     socket?: SocketIOClient.Socket
@@ -35,14 +38,29 @@ export const chatReducer = (state: IChatReducer = initialState, action: IChatAct
             return {
                 ...state,
                 commands: [...state.commands, command],
-                lastCommand: command
+                lastCommand: command,
+                conversation: [...state.conversation, { type: 'received', payload: command }]
             }
         }
         case MESSAGE_RECEIVED: {
             const message = action.payload as IMessageResponse
             return {
                 ...state,
-                messages: [...state.messages, message]
+                messages: [...state.messages, message],
+                conversation: [...state.conversation, { type: 'received', payload: message }]
+            }
+        }
+        case MESSAGE_SENT: {
+            const message = action.payload as IMessagePayload
+            return {
+                ...state,
+                conversation: [...state.conversation, { type: 'sent', payload: message }]
+            }
+        }
+        case COMMAND_SENT: {
+            return {
+                ...state,
+                conversation: [...state.conversation, { type: 'sent', payload: COMMAND_SENT }]
             }
         }
         default: {
